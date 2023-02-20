5f03d92be9a8a9234c456b239d24d69a20bfcf03
VenuChowdam <125816123+VenuChowdam@users.noreply.github.com>
2023-02-20 11:24:09 +0530

    Main class added

---
+++ src/test/java/sprintone/MainMenus.java
@@ -0,0 +1,14 @@
+package sprintone;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+public class MainMenus {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
